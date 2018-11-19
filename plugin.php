aaf3e37abb30e8d60615ef335ff5787ee170e2bd
JesseSijtsma <34576204+JesseSijtsma@users.noreply.github.com>
2018-11-19 21:40:34 +0100

    Add files via upload

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  JessePlug
+Plugin URI:   https://developer.wordpress.org/plugins/JessePlug/
+Description:  Basic WordPress Plugin Header Comment
+Version:      20160911
+Author:       WordPress.org
+Author URI:   https://developer.wordpress.org/JesseSijtsma
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
